27af65b8c726de310ad2285b3f48fa9bf70e3da5
Laura Catano <laura.n.catano@gmail.com>
2019-08-16 09:51:17 -0500

    initial commit!

---
+++ PizzaBox/PizzaBox.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PizzaBox.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ PizzaBox/PizzaBox.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PizzaBox.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
